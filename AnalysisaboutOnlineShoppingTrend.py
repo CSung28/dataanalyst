404e4b3533ca900def4e2d4492d0f88577c434da
CSung28 <vnfmsdbslzhs@gmail.com>
2022-11-21 19:47:32 +0900

    docs: 221121 Analysis about Online Shopping Trend
    Condition: Uploading my first personal Python project on Github.

---
+++ AnalysisaboutOnlineShoppingTrend.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#### Code that can use Korean on Seaborn, matplotlib on Mac
+import matplotlib.pyplot as plt
+from matplotlib import rc
+import seaborn as sns
+%matplotlib inline
+
+rc('font', family='AppleGothic')
+
+plt.rcParams['axes.unicode_minus'] = False
+
+# Theme 1. Analysis Online Shopping Trend in 2022.01-09 

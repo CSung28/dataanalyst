2ca4d725ae2ced8192c31c8675177924431dda23
CSung28 <vnfmsdbslzhs@gmail.com>
2022-11-22 18:48:43 +0900

    docs: 2201122 Data Load & Preprocessing
    Condition: Data Preprocessing will continue.

--- AnalysisaboutOnlineShoppingTrend.py
+++ AnalysisaboutOnlineShoppingTrend.py
@@ -13,4 +13,26 @@ rc('font', family='AppleGothic')
 
 plt.rcParams['axes.unicode_minus'] = False
 
-# Theme 1. Analysis Online Shopping Trend in 2022.01-09 
+# Theme 1. Analysis Online Shopping Trend in 2022.01-09
+
+# 1-1. Data load 
+
+df1 = pd.read_excel('file_path/file_name.xlsx', sheet_name = 0)
+
+# check data that it was brought well
+df1
+
+# check the columns 
+df1.columns
+
+# When i bring the data, there was unnecessary row on the first. 
+# so I delete it with 'Indexing'.
+df1 = df1[1:]
+
+# Then i re-check the info.
+df1.info()
+
+# I can see that unnecessary row was deleted!
+
+
+ 
